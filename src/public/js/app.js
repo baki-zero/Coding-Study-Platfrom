bd5a11dc94e60db83b3873394eac01479c824636
baki-zero <bakizero815@gmail.com>
2023-03-20 23:14:07 +0900

    Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,26 @@ const socket = io();   //socket.io를 이용해 front-end에서 back-end로 연결
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
+const cameraSelect = document.getElementById("cameras");
 //stream은 비디오와 오디오가 결합된 것
 let myStream;
 let muted = false;          //처음에 소리 x
 let cameraOff = false;      //처음에 카메라 on
+
+async function getCameras() {
+try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter(device => device.kind === "videoinput");
+    cameras.forEach(camera => {
+        const option = document.createElement("option");
+        option.value = camera.deviceId;
+        option.innerText = camera.label;
+        cameraSelect.appendChild(option);
+    })
+} catch(e) {
+    console.log(e);
+}
+}
 
 async function getMedia() {
     try {
@@ -15,6 +31,7 @@ async function getMedia() {
             video: true,        
         });
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.log(e);
     }
@@ -23,6 +40,7 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick() {
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
     if(!muted) {
         muteBtn.innerText = "Unmute";
         muted = true;
@@ -33,6 +51,7 @@ function handleMuteClick() {
 };
 
 function handleCameraClick() {
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
     if(cameraOff) {
         cameraBtn.innerText = "Turn Camera Off";
         cameraOff = false;
